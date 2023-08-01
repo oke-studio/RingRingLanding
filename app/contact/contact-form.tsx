90a5a42d38a1d7454dcab45e44b0d66dc2f38750
Daniel Odiase <82052211+DanielOdiase@users.noreply.github.com>
2023-08-01 09:16:12 -0400

    Update contact-form.tsx

--- app/contact/contact-form.tsx
+++ app/contact/contact-form.tsx
@@ -14,9 +14,9 @@ export default function ContactForm() {
     }
   return (
     <div className="  flex flex-col flex items-center md:flex-items-start w-full  h-full pb-20 ">
-          <form name="contact" data-netlify="true" method="POST" className='z-10 flex flex-col  md:pl-8 w-1/2  md:w-full ' data-netlify-honeypot="bot-field">
+          <form name="contact" data-netlify="true" method="POST" className='z-10 flex flex-col  md:pl-8 w-1/2  md:w-full ' data-netlify-honeypot="bot-field" autoComplete="off">
                 <input className='hidden' />
-                <input type="email" required placeholder='Enter your email Address' id="email" name="email"className=" bg-white rounded-md h-fit w-full text-center md:text-start md:w-4/5 p-2 mt-3 "/>
+                <input type="email" required placeholder='Enter your email Address'  name="email"className=" bg-white rounded-md h-fit w-full text-center md:text-start md:w-4/5 p-2 mt-3 "/>
                 <button type="submit" className='bg-[#BD4230] font-newhero font-normal text-white self-center md:self-start md:flex flex-start  p-3 border-b-4 border-b-[#5E2017] rounded-3xl w-fit mt-5 mb-7 '>Notify me for preorders </button>
                 </form>
               
